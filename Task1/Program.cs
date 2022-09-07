a3bb9162ccf58fdcedc4ba2ab785543cc7488759
Andrew <mr.fesenko@inbox.ru>
2022-09-07 17:21:23 +0300

    added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿double Clir(string text)
+{
+    Console.Write(text);
+    text = Console.ReadLine();
+    double result = double.Parse(text);
+    return result;
+}
+
+void GetNeededNumbers (double a, double b)
+{
+
+if (a == b) 
+{
+   return;
+}
+GetNeededNumbers(a, b-1);
+if (b%2==0)
+Console.WriteLine(b);
+}
+
+double a = Clir("Enter where the array starts ");
+double b = Clir("Enter where the array ends ");
+GetNeededNumbers(a,b);
