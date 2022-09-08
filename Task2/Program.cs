31956aca8188b398ffe68627e59eb15bbd6931b0
ФЕСС - <mr.fesenko@inbox.ru>
2022-09-08 18:51:13 +0300

    addeded Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿double Clir(string text)
+{
+    Console.Write(text);
+    text = Console.ReadLine();
+    double result = double.Parse(text);
+    return result;
+}
+
+void GetNeededSum (double a, double b, double sum)
+{
+if (a > b) 
+{
+    Console.WriteLine($"Sum of numbers in thus interval {sum} ");
+   return;
+}
+sum = sum + (a++);
+GetNeededSum(a, b, sum);
+
+}
+
+double a = Clir("Enter where the array starts ");
+double b = Clir("Enter where the array ends ");
+GetNeededSum(a,b,0);
